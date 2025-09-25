451ec6078aa86a8c45db89e05dcb2eb51c2c28a3
卜部 昌平 <s-urabe@sakura.ad.jp>
2025-09-25 15:55:48 +0900

    client and error
    Signed-off-by: 卜部 昌平 <s-urabe@sakura.ad.jp>

---
+++ helper_test.go
@@ -0,0 +1,75 @@
+// Copyright 2025- The sacloud/cloudhsm-api-go Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cloudhsm_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	client "github.com/sacloud/api-client-go"
+	. "github.com/sacloud/cloudhsm-api-go"
+	v1 "github.com/sacloud/cloudhsm-api-go/apis/v1"
+	"github.com/sacloud/packages-go/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func newTestClient(v any, s ...int) *v1.Client {
+	s = append(s, http.StatusOK)
+	j, e := json.Marshal(v)
+	if e != nil {
+		panic(e)
+	}
+
+	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		st := s[0]
+
+		w.WriteHeader(st)
+		if st == http.StatusNoContent {
+			return
+		}
+		if _, e = w.Write(j); e != nil {
+			panic(e)
+		}
+	})
+	sv := httptest.NewServer(h)
+	c, e := NewClientWithApiUrlAndClient(sv.URL, sv.Client())
+	if e != nil {
+		panic(e)
+	}
+	return c
+}
+
+func newIntegratedClient(t *testing.T, params ...client.ClientParam) *v1.Client {
+	testutil.PreCheckEnvsFunc(
+		"SAKURACLOUD_ACCESS_TOKEN",
+		"SAKURACLOUD_ACCESS_TOKEN_SECRET",
+	)(t)
+
+	apiUrl := DefaultAPIRootURL
+	if root, ok := os.LookupEnv("SAKURACLOUD_LOCAL_ENDPOINT_CLOUDHSM"); ok {
+		apiUrl = root
+	}
+	ret, err := NewClientWithApiUrl(apiUrl, append(params, client.WithApiKeys(
+		os.Getenv("SAKURACLOUD_ACCESS_TOKEN"),
+		os.Getenv("SAKURACLOUD_ACCESS_TOKEN_SECRET"),
+	))...)
+
+	require.NoError(t, err)
+	return ret
+}
