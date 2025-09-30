84829d04f98f6e463bafa2baf1116c5405e967af
卜部 昌平 <s-urabe@sakura.ad.jp>
2025-09-30 13:52:47 +0900

    actual zone
    Signed-off-by: 卜部 昌平 <s-urabe@sakura.ad.jp>

--- client_test.go
+++ client_test.go
@@ -27,7 +27,7 @@ func TestNewClient(t *testing.T) {
 }
 
 func TestNewClientWithApiUrl_OtherZone(t *testing.T) {
-	c, err := NewClientWithApiUrl("https://secure.sakura.ad.jp/cloud/zone/tk1b/api/monitoring/1.0/")
+	c, err := NewClientWithApiUrl("https://secure.sakura.ad.jp/cloud/zone/tk1a/api/cloud/1.1/")
 	require.NoError(t, err)
 	require.NotNil(t, c)
 }
